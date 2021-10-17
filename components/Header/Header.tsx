349144bd86a69613115b77bb835238dbe855ac75
Rei Krom <mr.mygo@gmail.com>
2021-10-17 22:40:09 +0100

    mobile header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 import Image from 'next/image';
 import Logo from '../../public/logo-shadow2x.png';
 import { Menu } from '@mui/icons-material';
+import { Input } from '@mui/material';
 
-const Header = () => {
+interface Props {}
+
+const header = 'flex items-center px-5';
+
+const Header = (props: Props) => {
     return (
-        <div style={{ border: '1px solid red' }}>
-            <Menu />
-            <Image src={Logo} height="24" width="84" />
+        <div className={`${header} h-11`}>
+            <Menu className="text-light-1 w-8 h-8" />
+            <div className="logo h-6 pl-4">
+                <Image src={Logo} height="24" width="84" />
+            </div>
+
+            <Input
+                id="search"
+                disableUnderline
+                className="text-light-4 border border-light-4 py-1 px-2 w-32 ml-auto bg-dark-3 h-8"
+                size="small"
+                placeholder="Search"
+                type="Search"
+            />
         </div>
     );
 };
